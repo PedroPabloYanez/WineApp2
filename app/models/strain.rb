500d25c17975606138e696b0ce339b510afb1879
pedro <pedropablo.yanez@hotmail.com>
2021-06-07 20:33:16 -0400

    first commit

---
+++ app/models/strain.rb
@@ -0,0 +1,17 @@
+class Strain < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+
+    has_many :wine_strains
+    has_many :wines, through: :wine_strains, dependent: :destroy
+    
+
+    def availability
+        if available == true
+            "Yes"
+        else
+            "No"
+        end
+    end
+
+
+end
---
+++ app/models/wine.rb
@@ -0,0 +1,48 @@
+class Wine < ApplicationRecord
+    has_many :wine_strains
+    has_many :strains, through: :wine_strains, dependent: :destroy
+    has_many :wine_oenologists, dependent: :destroy
+    has_many :oenologists, through: :wine_oenologists, dependent: :destroy
+
+    def addStrainPercent(percents)
+        percents.each do |strain_id, percentage|
+            if percentage != ''
+                temp_strain = self.wine_strains.where(strain_id: strain_id).first
+                temp_strain.percentage = percentage
+                temp_strain.save
+            end
+        end 
+    end
+
+    def strainnames_and_percentages
+        list = []
+        newe = []
+        wine_strains.map do |ws|
+            list << [ws.strain.name, ws.percentage ]
+            # "#{ws.strain.name} (#{ws.percentage}%)"
+        end
+        list.sort_by{ |strain| strain[0] }.each do |wso|
+            newe << "#{wso[0]}  (#{wso[1]}%)"
+        end
+        newe.join(', ')
+    end 
+
+    def get_oenologist
+        list = []
+        newe = []
+        oenologists.map do |ws|
+            list << [ws.name, ws.age ]
+        end
+        list.sort_by{ |oeno| oeno[0] }.each do |wso|
+            newe << "#{wso[0]}  (#{wso[1]} year)"
+        end
+        newe.sort.join(', ')
+    end
+    
+    #def get_oenologist
+     #   oenologists.map do |oe_name| 
+      #      oe_name.name
+       # end.join(',  ')
+    #end
+
+end
